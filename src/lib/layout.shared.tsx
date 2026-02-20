374e09d08e1f26b660004eb8460abca23f9b555b
Prakrit M. <prakritmanstudio@gmail.com>
2026-02-20 15:11:44 +0700

    โครงสร้าง eoffice พื้นฐาน

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,16 +1,16 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
-  branch: 'main',
+  user: "gdcc-service",
+  repo: "gdcc-eoffice-docs",
+  branch: "main",
 };
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
+      title: "e-Office Documentation",
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
