65cb64459cc2840fd21c59a0ad37ca103bd5e9b6
Prakrit M. <prakritmanstudio@gmail.com>
2026-02-20 15:12:01 +0700

    icon eoffice

---
+++ src/lib/source.tsx
@@ -0,0 +1,70 @@
+import { docs } from "fumadocs-mdx:collections/server";
+import { type InferPageType, loader } from "fumadocs-core/source";
+import { lucideIconsPlugin } from "fumadocs-core/source/lucide-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFirstdraft } from "@fortawesome/free-brands-svg-icons";
+import {
+  faHouse,
+  faScrewdriverWrench,
+  faBook,
+  faFileSignature,
+} from "@fortawesome/free-solid-svg-icons";
+
+// See https://fumadocs.dev/docs/headless/source-api for more info
+export const source = loader({
+  baseUrl: "/docs",
+  source: docs.toFumadocsSource(),
+  // plugins: [lucideIconsPlugin()],
+  icon(icon) {
+    switch (icon) {
+      case "portal": {
+        return <FontAwesomeIcon icon={faHouse} style={{ color: "#177fff" }} />;
+      }
+      case "admin-tools": {
+        return (
+          <FontAwesomeIcon
+            icon={faScrewdriverWrench}
+            style={{ color: "#fecb3e" }}
+          />
+        );
+      }
+      case "draft": {
+        return (
+          <FontAwesomeIcon
+            icon={faFirstdraft}
+            style={{ color: "rgb(253, 126, 20)" }}
+          />
+        );
+      }
+      case "saraban": {
+        return <FontAwesomeIcon icon={faBook} style={{ color: "#ff8080" }} />;
+      }
+      case "in-tray": {
+        return (
+          <FontAwesomeIcon
+            icon={faFileSignature}
+            style={{ color: "#c68357" }}
+          />
+        );
+      }
+    }
+    return undefined;
+  },
+});
+
+export function getPageImage(page: InferPageType<typeof source>) {
+  const segments = [...page.slugs, "image.png"];
+
+  return {
+    segments,
+    url: `/og/docs/${segments.join("/")}`,
+  };
+}
+
+export async function getLLMText(page: InferPageType<typeof source>) {
+  const processed = await page.data.getText("processed");
+
+  return `# ${page.data.title}
+
+${processed}`;
+}
